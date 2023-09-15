ca6787c3553842844d95d705ec288691c12b1df2
Bartosz Kasprzyk <bartosz.kasprzyk58@gmail.com>
2023-09-15 21:14:15 +0200

    added "show/hide done"& "toggle done all" buttons

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 {
-    const tasks = [];
+    let tasks = [];
+    let hideDoneTasks = false;
+
+    const showOrHideDoneTasks = () => {
+        hideDoneTasks = !hideDoneTasks;
+        render();
+    };
+
+    const markAllTasksDone = () => {
+        tasks = tasks.map((task) => ({ ...task, done: true }));
+        render();
+    };
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-
+        tasks = [
+            ...tasks,
+            { content: newTaskContent },
+        ];
         render();
     };
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = tasks.filter((task, index) => index !== taskIndex);
         render();
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = tasks.map((task, index) => index === taskIndex ? { ...task, done: !tasks[taskIndex].done } : task);
         render();
     };
 
@@ -37,26 +48,59 @@
         });
     }
 
-    const render = () => {
+    const renderTasks = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-                <li class="tasks__item ${task.done ? "tasks__item--done" : ""}">
-                    <button class="js-done tasks__button tasks__button--done">
-                        <i class="${task.done ? "gg-check" : "tasks__button--disabled"}"></i>
-                    </button>
-                    <span class="tasks__content">${task.content}</span>
-                    <button class="js-remove tasks__button tasks__button--remove">
-                        <i class="gg-trash"></i>
-                    </button>
-                </li>
-        `;
+            <li class="tasks__item ${task.done && hideDoneTasks ? "tasks__hidden" : ""}">
+                <button class="js-done tasks__button tasks__button--done">
+                    <i class="${task.done ? "gg-check" : "tasks__button--disabled"}"></i>
+                </button>
+                <span class="tasks__content ${task.done ? "tasks__item--done" : ""}">${task.content}</span>
+                <button class="js-remove tasks__button tasks__button--remove">
+                    <i class="gg-trash"></i>
+                </button>
+            </li>
+    `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const renderButtons = () => {
+        const buttonsElement = document.querySelector(".js-buttons");
+        if (!tasks.length) {
+            buttonsElement.innerHTML = ``;
+            return;
+        }
+        buttonsElement.innerHTML = `
+            <button class="section__button js-showOrHideDone">${hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone</button>
+            <button class="section__button ${tasks.every(({ done }) => done) ? "section__button--disabled" : ""} js-allTasksDone">Ukończ wszystkie</button>
+            `;
+    };
+
+    const bindButtonsEvents = () => {
+        const toggleShowHideButton = document.querySelector(".js-showOrHideDone");
+
+        if (toggleShowHideButton) {
+            toggleShowHideButton.addEventListener("click", showOrHideDoneTasks);
+        };
+
+        const markAllTasksDoneButton = document.querySelector(".js-allTasksDone");
+
+
+        if (markAllTasksDoneButton) {
+            markAllTasksDoneButton.addEventListener("click", markAllTasksDone);
+        };
+    };
+
+    const render = () => {
+        renderTasks();
+        renderButtons();
 
         bindEvents();
+        bindButtonsEvents();
     };
 
     const onFormSubmit = (event) => {
